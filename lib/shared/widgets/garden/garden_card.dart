08a8152ee09381114fe8a78fbc85eac0e99fd932
DESKTOP-ANTON\utzan <alien-code@outlook.de>
2022-04-04 18:04:47 +0200

    HP-32: Added long press, modified gardens widget

--- lib/shared/widgets/garden/garden_card.dart
+++ lib/shared/widgets/garden/garden_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:happy_plants/shared/models/garden.dart';
 
@@ -14,9 +15,8 @@ class _GardenSingleState extends State<GardenSingle> {
   @override
   Widget build(BuildContext context) {
     var stringOfImageName = 'assets/images/garden_backgrounds/one.jpg';
-    Image imageAsWidget = Image.asset(
+    AssetImage imageAsWidget = AssetImage(
       stringOfImageName,
-      fit: BoxFit.cover,
     );
     //check if string of filename is known, if yes paste it in path
     if(Garden.checkItemName(widget.garden.icon)) {
@@ -24,75 +24,74 @@ class _GardenSingleState extends State<GardenSingle> {
       'assets/images/garden_backgrounds/${widget.garden.icon}.jpg';
       //try to access picture in path created
       try {
-        imageAsWidget = Image.asset(
-          stringOfImageName,
-          fit: BoxFit.cover,
-        );
+        imageAsWidget = AssetImage(stringOfImageName);
       } catch (e) {
-        imageAsWidget = Image.asset(
-          'assets/images/garden_backgrounds/one.jpg',
-          fit: BoxFit.cover,
-        );
+        imageAsWidget = const AssetImage('assets/images/garden_backgrounds/one.jpg');
       }
     }
     else{
-      imageAsWidget = Image.asset(
-        'assets/images/garden_backgrounds/one.jpg',
-        fit: BoxFit.cover,
-      );
+      imageAsWidget = const AssetImage('assets/images/garden_backgrounds/one.jpg');
     }
 
-    return GestureDetector(
-      onTap: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(widget.garden.name + ' was clicked!')),
-        );
-      },
-      child: Card(
-        margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-        //Todo: not working yet :D (wird überschrieben von irgendwas)
-        shape: BeveledRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 200.0,
-              child: Stack(
-                children: <Widget>[
-                  Positioned.fill(
-                    child: imageAsWidget
+    return CupertinoContextMenu(
+        child: GestureDetector(
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(widget.garden.name + ' was clicked!')),
+              );
+            },
+            child:  Card(
+                semanticContainer: true,
+                margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0)
+                ),
+                //Todo: not working yet :D (wird überschrieben von irgendwas)
+                child: Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                          image: imageAsWidget,
+                          fit: BoxFit.cover
+                      )
                   ),
-                  Positioned(
-                    bottom: 16.0,
-                    left: 16.0,
-                    right: 16.0,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 0, 0, 16),
                     child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      alignment: Alignment.centerLeft,
-                      child: Card(
-                        color: Colors.black45,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                        child: Text(
-                          widget.garden.name,
-                          softWrap: true,
-                          style: Theme.of(context).textTheme.headline5!.copyWith(
-                            color: Colors.white,
-                          ),
+                    fit: BoxFit.scaleDown,
+                    alignment: Alignment.bottomLeft,
+                    child: Card(
+                      color: Colors.black45,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      child: Text(
+                        widget.garden.name,
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.headline5!.copyWith(
+                          color: Colors.white,
                         ),
                       ),
                     ),
                   )
-                ],
-              ),
+                ),
+              )
             ),
-          ],
         ),
-      )
+        actions: <Widget>[
+          CupertinoContextMenuAction(
+            child: const Text("Delete Garden"),
+            onPressed: (){
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text(widget.garden.id + ' was popup deleted!')));
+            },
+          ),
+        ],
     );
+
+
+
   }
 }
 
