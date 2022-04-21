62b9ab0b1e0ede5079cfb80e2dc7f4f184a65d3e
Tom Freudenmann <tom.freudenmann@outlook.de>
2022-04-21 15:42:49 +0200

    HP-44: finished drop down

--- lib/shared/widgets/forms/custom_dropdown.dart
+++ lib/shared/widgets/forms/custom_dropdown.dart
@@ -1,36 +1,72 @@
 import 'package:flutter/material.dart';
 
-class CustomSelect extends StatefulWidget {
-  const CustomSelect({
+import '../../utilities/app_colors.dart';
+
+class CustomDropDown extends StatefulWidget {
+  const CustomDropDown({
     Key? key,
     required this.menuItems,
     required this.title,
     required this.onChange,
-    this.hint
+    required this.hint,
+    this.icon,
+    this.validator,
   }) : super(key: key);
 
   final List<String> menuItems;
   final String title;
   final Function(String? newValue) onChange;
-  final String? hint;
+  final String hint;
+  final IconData? icon;
+  final String? Function(String? value)? validator;
 
   @override
-  State<CustomSelect> createState() => _CustomSelectState();
+  State<CustomDropDown> createState() => _CustomDropDownState();
 }
 
-class _CustomSelectState extends State<CustomSelect> {
+class _CustomDropDownState extends State<CustomDropDown> {
 
   //dropdown buttons choices
-  String? _buttonPotSizeSelected;
+  String? _currentValue;
+  Widget? suffixIcon;
+
+  final FocusNode _focus = FocusNode();
+
+  /// Actualizes on focus changed the state of this widget
+  void _onFocusChange() {
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _focus.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _focus.removeListener(_onFocusChange);
+    _focus.dispose();
+  }
+
+  /// Generates the color of the suffix icon
+  Color? _getIconColor(){
+    if(_focus.hasFocus){
+      return AppColors.accent1;
+    }
+
+    return null;
+  }
+
 
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
+    ThemeData theme = Theme.of(context);
 
-    Widget? hint;
-
-    if(widget.hint != null){
-      hint = Text(widget.hint!);
+    if(widget.icon != null) {
+      suffixIcon = Icon(widget.icon, color: _getIconColor());
     }
 
     // Map input menu items to local format
@@ -42,18 +78,22 @@ class _CustomSelectState extends State<CustomSelect> {
     ).toList();
 
 
-    return Row(
-      children: <Widget>[
-        Text(widget.title, style: textTheme.bodyText1),
-        DropdownButton(
-          value: _buttonPotSizeSelected,
-          hint: hint,
-          onChanged: (String? newValue) {
-            widget.onChange(newValue);
-          },
-          items: _mappedMenuItems,
-        ),
-      ],
+    return DropdownButtonFormField(
+      items: _mappedMenuItems,
+      onChanged: (String? newValue) {
+        widget.onChange(newValue!);
+        _currentValue = newValue;
+      },
+      value: _currentValue,
+      validator: widget.validator,
+      enableFeedback: true,
+      focusNode: _focus,
+      decoration: InputDecoration(
+        focusColor: AppColors.accent1,
+        hintText: widget.hint,
+        labelText: widget.title,
+        suffixIcon: suffixIcon,
+      ),
     );
 
   }
