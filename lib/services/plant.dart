da1c69358ab8c64dfc9f2e12d833453c6174dadb
Tom Freudenmann <tom.freudenmann@outlook.de>
2022-04-30 16:10:00 +0200

    HP-22: delete notification and event on garden delte

--- lib/services/plant.dart
+++ lib/services/plant.dart
@@ -1,9 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:happy_plants/config.dart';
-import 'package:happy_plants/screens/home/tabs/garden/plants/new_plant.dart';
 import 'package:happy_plants/services/event.dart';
-import 'package:happy_plants/services/notification.dart';
 import 'package:happy_plants/shared/models/events.dart';
 import 'package:happy_plants/shared/models/plant.dart';
 import 'package:happy_plants/shared/models/user.dart';
