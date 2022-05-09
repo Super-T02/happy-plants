97ad6fa12424e618528361176625c0950e2dc487
DESKTOP-ANTON\utzan <alien-code@outlook.de>
2022-05-09 18:11:28 +0200

    HP-59: costum_list_row correction, made data not required
    (cherry picked from commit 9cc705da6c20bff7ace2846914f6a21467a18753)

--- lib/shared/widgets/util/lists/custom_list_row.dart
+++ lib/shared/widgets/util/lists/custom_list_row.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CustomListRow extends StatefulWidget {
-  const CustomListRow({Key? key, required this.title, required this.data}) : super(key: key);
+  const CustomListRow({Key? key, required this.title, this.data}) : super(key: key);
 
   final String title;
-  final String data;
+  final String? data;
 
   @override
   State<CustomListRow> createState() => _CustomListRowState();
@@ -14,6 +14,26 @@ class _CustomListRowState extends State<CustomListRow> {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+    Widget data;
+    if(widget.data != null && widget.data != 'null'){
+      data = Expanded(child:
+        Text(
+          widget.data!,
+          style: theme.textTheme.subtitle2,
+          textAlign: TextAlign.right,
+        ),
+      );
+    }
+    else{
+      data = Expanded(child:
+        Text(
+          '-',
+          style: theme.textTheme.subtitle2,
+          textAlign: TextAlign.right,
+        ),
+      );
+    }
+
     return Container(
       decoration: BoxDecoration(
         border:  Border(
@@ -26,13 +46,7 @@ class _CustomListRowState extends State<CustomListRow> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(widget.title, style: theme.textTheme.bodyText1),
-          Expanded(child:
-            Text(
-              widget.data,
-              style: theme.textTheme.subtitle2,
-              textAlign: TextAlign.right,
-            ),
-          ),
+          data,
         ],
       )
     );
